d9800e8114cd7fa9db0aefd2d828db7d7b4bbe6c
federicofrontera <federicofrontera@gmail.com>
2021-01-16 10:02:54 -0300

    add some module based css for Layout component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Auxiliary from "../../hoc/Auxiliary";
 
+import styles from './Layout.module.css'
+
 const layout = (props) => (
     <Auxiliary>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className={styles.Content}>
             {props.children}
         </main>
     </Auxiliary>
