cc4761a29ec7d68f7c8c6a9eff62be508193de02
federicofrontera <federicofrontera@gmail.com>
2021-01-16 21:36:15 -0300

    only show modal after going to checkout

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -3,7 +3,11 @@ import styles from './Modal.module.css'
 
 function Modal(props) {
     return (
-        <div className={styles.Modal}>
+        <div className={styles.Modal}
+                style={{
+                    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity: props.show ? '1' : '0'
+                }}>
             {props.children}
         </div>
     );
