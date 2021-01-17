e736370093d51a9bf83842abce94cce2da512e5c
federicofrontera <federicofrontera@gmail.com>
2021-01-17 11:36:03 -0300

    fix unnecessary updating of modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -3,20 +3,26 @@ import styles from './Modal.module.css';
 import Auxiliary from "../../../hoc/Auxiliary";
 import Backdrop from "../Backdrop/Backdrop";
 
-function Modal(props) {
-    return (
-        <Auxiliary>
-            <Backdrop show={props.show} clicked={props.modalClosed}/>
-            <div className={styles.Modal}
-                 style={{
-                     transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                     opacity: props.show ? '1' : '0'
-                 }}>
-                {props.children}
-            </div>
-        </Auxiliary>
+class Modal extends React.Component {
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+       return nextProps.show !== this.props.show;
+    }
 
-    );
+    render() {
+        return (
+            <Auxiliary>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+                <div className={styles.Modal}
+                     style={{
+                         transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                         opacity: this.props.show ? '1' : '0'
+                     }}>
+                    {this.props.children}
+                </div>
+            </Auxiliary>
+
+        );
+    }
 }
 
 export default Modal;
