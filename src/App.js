fb75d92cb630fb06dcff48d3efee726c2b4c7abd
federicofrontera <federicofrontera@gmail.com>
2021-01-20 17:52:49 -0300

    add routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+import {Component} from "react";
+import { Route, Switch } from 'react-router-dom'
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
-import {Component} from "react";
 import Checkout from "./containers/Checkout/Checkout";
 
 class App extends Component {
@@ -8,8 +9,10 @@ class App extends Component {
         return (
             <div>
                 <Layout>
-                    <BurgerBuilder/>
-                    <Checkout/>
+                    <Switch>
+                        <Route path="/checkout" component={Checkout}/>
+                        <Route path="/" exact component={BurgerBuilder}/>
+                    </Switch>
                 </Layout>
             </div>
         );
