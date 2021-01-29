28a1bdf14c544d0e8a31dc508e5232227c4d8ce9
federicofrontera <federicofrontera@gmail.com>
2021-01-29 17:43:27 -0300

    add Auth form and routing

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 
 import Input from '../../components/UI/Input/Input'
 import Button from '../../components/UI/Button/Button'
+import styles from './Auth.module.css'
 
 class Auth extends Component {
     state = {
@@ -29,7 +30,7 @@ class Auth extends Component {
                 value: '',
                 validation: {
                     required: true,
-                    mingLength: 6
+                    minLength: 6
                 },
                 valid: false,
                 touched: false
@@ -37,12 +38,57 @@ class Auth extends Component {
         }
     }
 
+    inputChangedHandler = (event, controlName) => {
+        const updatedControls = {
+            ...this.state.controls,
+            [controlName]: {
+                ...this.state.controls[controlName],
+                value: event.target.value,
+                valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
+                touched: true
+            }
+        };
+        this.setState({controls: updatedControls})
+    }
+
+    checkValidity(value, rules) {
+        let isValid = true;
+        if (!rules) {
+            return true;
+        }
+
+        if (rules.required) {
+            isValid = value.trim() !== '' && isValid;
+        }
+
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid
+        }
+
+        if (rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid
+        }
+
+        if (rules.isEmail) {
+            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+            isValid = pattern.test(value) && isValid
+        }
+
+        if (rules.isNumeric) {
+            const pattern = /^\d+$/;
+            isValid = pattern.test(value) && isValid
+        }
+
+        return isValid;
+    }
+
+
     render() {
         const formElementsArray = [];
-        for (let key in this.state.orderForm) {
+        for (let key in this.state.controls) {
             formElementsArray.push({
                 id: key,
-                config: this.state.orderForm[key]
+                config: this.state.controls[key]
             });
         }
         const form = formElementsArray.map(formElement => (
@@ -61,7 +107,7 @@ class Auth extends Component {
 
         return (
 
-            <div>
+            <div className={styles.Auth}>
                 <form action="">
                     {form}
                     <Button btnType="Success">SUBMIT</Button>
