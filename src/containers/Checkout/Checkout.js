9b253b4c949f579701267ba0379d2ba628824c74
federicofrontera <federicofrontera@gmail.com>
2021-01-29 11:42:05 -0300

    fix error when refreshing checkout

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import {Route} from 'react-router-dom'
-import { connect } from 'react-redux';
+import {Route, Redirect} from 'react-router-dom'
+import {connect} from 'react-redux';
 import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
 import ContactData from "./ContactData/ContactData";
 
@@ -11,21 +11,25 @@ class Checkout extends Component {
     }
 
     checkoutContinuedHandler = () => {
-        this.props.history.replace( '/checkout/contact-data' );
+        this.props.history.replace('/checkout/contact-data');
     }
 
-    render () {
-        return (
-            <div>
-                <CheckoutSummary
-                    ingredients={this.props.ingredients}
-                    checkoutCancelled={this.checkoutCancelledHandler}
-                    checkoutContinued={this.checkoutContinuedHandler} />
-                <Route
-                    path={this.props.match.path + '/contact-data'}
-                    component={ContactData} />
-            </div>
-        );
+    render() {
+        let summary = <Redirect to="/"/>
+        if (this.props.ingredients) {
+            summary =
+                <div>
+                    <CheckoutSummary
+                        ingredients={this.props.ingredients}
+                        checkoutCancelled={this.checkoutCancelledHandler}
+                        checkoutContinued={this.checkoutContinuedHandler}/>
+                    <Route
+                        path={this.props.match.path + '/contact-data'}
+                        component={ContactData}/>
+                </div>
+
+        }
+        return summary;
     }
 }
 
