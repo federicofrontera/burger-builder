5e8c836e2b98b783cecd6f259125683305dfd302
federicofrontera <federicofrontera@gmail.com>
2021-01-30 10:42:32 -0300

    hidde orders if not logged int

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,7 +6,7 @@ function NavigationItems(props) {
     return (
         <ul className={styles.NavigationItems}>
             <NavigationItem link="/" exact >Burger Builder</NavigationItem>
-            <NavigationItem link="/orders">Orders</NavigationItem>
+            {props.isAuthenticated ? <NavigationItem link="/orders">Orders</NavigationItem> : null}
             {!props.isAuthenticated
                 ? <NavigationItem link="/auth">Authenticate</NavigationItem>
                 : <NavigationItem link="/logout">Logout</NavigationItem>}
