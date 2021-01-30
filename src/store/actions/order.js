5f39acf16da0d620443035023e895eb1328c59fa
federicofrontera <federicofrontera@gmail.com>
2021-01-30 17:45:38 -0300

    guard routes

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -61,10 +61,16 @@ export const fetchOrdersStart = () => {
     }
 }
 
-export const fetchOrders = (token = null) => {
+export const fetchOrders = (token = null, userId) => {
     return dispatch => {
         dispatch(fetchOrdersStart());
-        axios.get(`/orders.json?auth=${token}`)
+        axios.get('orders.json', {
+            params: {
+                auth: token,
+                orderBy: '"userId"',
+                equalTo: `"${userId}"`
+            }
+        })
             .then(res => {
                 const fetchedOrders = [];
                 for (let key in res.data) {
